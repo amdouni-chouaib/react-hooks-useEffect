9041112f471593c0bb54a64101d9087d3d2e4c0a
amdouni-chouaib <chouaibamdouni@gmail.com>
2021-08-24 01:42:18 +0100

    react hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [windowswidth, setWindowswidth] = useState(window.innerWidth);
+  const [windowsheigth, setWindowsheigth] = useState(window.innerHeight);
+  const resizing = () => {
+    setWindowswidth(window.innerWidth);
+    setWindowsheigth(window.innerHeight);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", resizing);
+  }, []);
+  // in those cotes [] in the useEffect function you put the condition to run :
+  //window.addEventListener("resize", resizing);
+  //but in our case we doesn't have condition because we have an eventlistener
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <center>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "red",
+            border: "2",
+            borderRadius: "25px",
+            marginTop: "20%",
+            width: "20%",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {windowswidth}
+        </h1>
+      </center>
+      <center>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "red",
+            border: "2",
+            borderRadius: "25px",
+            marginTop: "20%",
+            width: "20%",
+          }}
+        >
+          {windowsheigth}
+        </h1>
+      </center>
     </div>
   );
 }
